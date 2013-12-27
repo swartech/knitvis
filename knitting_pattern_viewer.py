ae369ac08615461d1b9e0a04e0a3f5f8395dced7
swartech <whelans1.cs.man.ac.uk>
2013-12-27 12:57:20 +0000

    fully functional ()* repeats with borders

--- knitting_pattern_viewer.py
+++ knitting_pattern_viewer.py
@@ -42,6 +42,8 @@ print()
 for r in range(0, number_of_rows):
     temp = []
     repeats = []
+    end = []
+    after_brackets = False
     in_brackets = False
     for s in range(0, len(expression[r])):
         this_string = expression[r][s]
@@ -53,20 +55,35 @@ for r in range(0, number_of_rows):
         if len(this_string) > 1:
             foo = int(re.findall(r'\d+', this_string)[0])
             for i in range(foo):
-                temp.append(this_string[0])
                 if in_brackets:
                     repeats.append(this_string[0])
+                elif after_brackets:
+                    end.append(this_string[0])
+                else:
+                    temp.append(this_string[0])
+        #if just K or P
         else:
-            temp.append(this_string)
             if in_brackets:
-                repeats.append(this_string[0])
+                repeats.append(this_string)
+            elif after_brackets:
+                end.append(this_string)
+            else:
+                temp.append(this_string)
+        #if the end of a repeat
         if this_string[-1] == '*':
             in_brackets = False
-    pattern.append(temp)
-    print(repeats)
-    while len(pattern[r]) < length_of_row:
+            after_brackets = True
+
+    #repeat the pattern until the end border
+    repeat_limit = length_of_row - len(end)
+    while len(temp) < repeat_limit:
         for i in repeats:
-            pattern[r].append(i)
+            temp.append(i)
+    for e in end:
+        temp.append(e)
+
+    #add this row to the pattern
+    pattern.append(temp)
 
 #print interpreted pattern
 print("INTERPRETED PATTERN")
@@ -102,11 +119,11 @@ while True:
     x = 0
 
     #draw stitches
-    if show_wrong_side == True:
+    if show_wrong_side:
         for r in range(number_of_rows):
             #if working on the wrong side going left to right
             if r % 2 == 0:
-                for s in range(0,length_of_row):
+                for s in range(0, length_of_row):
                     draw_stitch(wrong_side(pattern[r][s]), x, y)
                     x += stitch_width
             else:
@@ -121,7 +138,7 @@ while True:
         for r in range(number_of_rows):
             #if working on the wrong side going left to right
             if r % 2 != 0:
-                for s in range(0,length_of_row):
+                for s in range(0, length_of_row):
                     draw_stitch(wrong_side(pattern[r][s]), x, y)
                     x += stitch_width
             else:
@@ -141,7 +158,7 @@ while True:
         #if space is pressed swap sides
         if event.type == KEYDOWN:
             if event.key == K_SPACE:
-                if show_wrong_side == True:
+                if show_wrong_side:
                     show_wrong_side = False
                     pygame.display.set_caption('Knitting Pattern Visualiser - RIGHT SIDE')
                 else:
